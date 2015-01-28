86e42c090dbb24f368008af143e037babf455376
Punit <punitdhoot1@gmail.com>
2015-01-28 18:57:35 +0530

    About me site

---
+++ nav.js
@@ -0,0 +1,8 @@
+document.getElementById("nav01").innerHTML = 
+"<ul id='navbar'>"+ 
+"<li><a href ='index.html'>Home</a></li>"+
+"<li><a href='Interests.html'>Interests</a></li>"+
+"<li><a href='ana.html'>Aims and Aspirations</a></li>"+
+"<li><a href = 'contact.html'>Contact Me</a></li>"+
+"</ul>";
+document.getElementById("foot").innerHTML = "<p>&copy; Punit Dhoot SDSLabs 2015</p>";
